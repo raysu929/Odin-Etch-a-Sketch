6b57bdff5748c9713ed3a581824a5d2b0fa2dffa
Raeesah Moola <raeesahmoola529@gmail.com>
2025-04-17 16:59:56 +0200

    Add black hover effect for Etch A Sketch-style drawing

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ const gridSize = 16;
 for(let i = 0; i < gridSize * gridSize; i++) {
   const square = document.createElement("div");
   square.classList.add("grid-square");
+
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'black';
+  });
+
   container.appendChild(square);
 }
